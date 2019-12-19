0d819241fa526429e7944ac38b9f3d778e59dc19
prabhadahal <59045569+prabhadahal@users.noreply.github.com>
2019-12-19 14:49:21 +0545

    Update animation.js

--- js/animation.js
+++ js/animation.js
@@ -1,3 +1,7 @@
 $(window).on('load', () => {
     $('.spinner-wrapper').fadeOut('slow');
 })
+$(document).ready(() => {
+    console.log($('footer p'));
+    $('footer p').html = `&copy; ${new Date().getFullYear} Prabha Dahal All Rights Reserved`;
+})
