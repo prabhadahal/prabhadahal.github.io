dd22ad6e4a9703a5981c44e276856860d22c1013
prabhadahal <59045569+prabhadahal@users.noreply.github.com>
2019-12-20 21:21:33 +0545

    Update animation.js

--- js/animation.js
+++ js/animation.js
@@ -5,3 +5,33 @@ $(document).ready(() => {
     console.log($('footer p'));
     $('footer p').html = `&copy; ${new Date().getFullYear} Prabha Dahal All Rights Reserved`;
 })
+/*progress bar animation on scroll*/
+$(window).on("scroll", function () {
+    if ($(this).scrollTop() > 1100) {
+        $('.outer-bar').addClass('animate-progress');
+        $(window).off("scroll"); // last line
+    }
+    console.log(pageYOffset);
+});
+/*menu*/
+$('.menu-btn').on('click', () => {
+    $('.menu-items').toggleClass('toggle-scale');
+
+})
+
+/*smooth scroll*/
+//smooth scrool
+
+
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+
+    event.preventDefault();
+
+    $('html, body').animate({
+
+        scrollTop: $($.attr(this, 'href')).offset().top
+
+    }, 800);
+
+});
